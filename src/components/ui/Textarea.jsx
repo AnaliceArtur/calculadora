263f781ad1d55291aeb0274ec693f3a5ed0bf026
AnaliceArtur <fitswapfinal@gmail.com>
2025-11-27 18:07:25 -0300

    Add files via upload

---
+++ src/components/ui/Textarea.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export function Textarea({ className = "", ...props }) {
+  return (
+    <textarea
+      className={w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-[#FF6B35] focus:border-transparent ${className}}
+      {...props}
+    />
+  );
+}
+
+export default Textarea;
